6c718ebcf7901616c0db3cde27f2e86ad5e28252
BrianDYKim <18k7102dy@naver.com>
2022-09-17 20:58:19 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/mongodbwebfluxplayground/MongodbWebfluxPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mongodbwebfluxplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongodbWebfluxPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<MongodbWebfluxPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mongodbwebfluxplayground/MongodbWebfluxPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mongodbwebfluxplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongodbWebfluxPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
